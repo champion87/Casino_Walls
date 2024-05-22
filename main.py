1df07e4b32a0d5c4b54f23b33845e9c9e8070dba
daniel-rosenfeld <rosenfeld5@mail.tau.ac.il>
2024-05-22 17:10:48 +0300

    daniel: takes coin amount form server

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import random
 import string
 
 API_KEYS = {}
+KEYS_TO_COINS = {}
 app = FastAPI()
 
 
 api_key_query = APIKeyQuery(name="api-key", auto_error=False)
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
-
+ 
 def key_gen():
     result_str = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
     return result_str
@@ -23,18 +24,6 @@ def get_api_key(
     api_key_query: str = Security(api_key_query),
     api_key_header: str = Security(api_key_header),
 ) -> str:
-    """Retrieve and validate an API key from the query parameters or HTTP header.
-
-    Args:
-        api_key_query: The API key passed as a query parameter.
-        api_key_header: The API key passed in the HTTP header.
-
-    Returns:
-        The validated API key.
-
-    Raises:
-        HTTPException: If the API key is invalid or missing.
-    """
     print(api_key_query)
 
     if api_key_query in API_KEYS.values():
@@ -48,9 +37,6 @@ def get_api_key(
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request : Request):
-    client_ip = request.client
-    my_api_key = key_gen()
-    API_KEYS[client_ip] = my_api_key
     return FileResponse('HTML_files/root_page.html')
 
 
@@ -81,5 +67,17 @@ async def read_games(api_key: str = Security(get_api_key)):
 
 @app.get("/get_my_api_key/")
 async def read_my_api_key(request : Request):
-    client_ip = request.client
+    client_ip = request.client.host
     return API_KEYS[client_ip]
+
+@app.get("/get_coin_amount/{key}")
+async def get_coin_amount(key):
+    return str(KEYS_TO_COINS[key])
+
+@app.get("/create_guest_acount/")
+async def create_guest_acount(request : Request):
+    client_ip = request.client.host
+    my_api_key = key_gen()
+    API_KEYS[client_ip] = my_api_key
+    KEYS_TO_COINS[my_api_key] = 100
+    return my_api_key
