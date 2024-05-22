6c0b7874331ea93212fe818c488a43543a7db482
champion8 <lidor071205@gmail.com>
2024-05-22 11:28:55 +0300

    nothing important

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,5 +12,9 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/games/wheel_of_fortune", response_class=)
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
