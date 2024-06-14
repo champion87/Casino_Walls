6e914b3a7d665e988d2e9b70a03ea654cd86b374
champion8 <lidor071205@gmail.com>
2024-06-14 17:33:03 +0300

    added all functionality to lobby for now

--- backend/logics/games.py
+++ backend/logics/games.py
@@ -7,20 +7,15 @@ from logics.game import Game
 from logics.card_game import BlackJack
 from utils.my_log import LOG
 
-SESSIONS : Dict[str , Game] = {"id" : Game()}
 
 GAMES : Dict[str , Game] = {
     "blackjack" : BlackJack
 }
 
-def get_session(session_key: str = Path()):
-    LOG(f"got {session_key = }")
-    return SESSIONS[session_key]
+
 
 # @return None - if no such game was found at 'games::GAMES'
 def get_game(game_name: str = Query()):
     LOG(f"got {game_name = }")
     return GAMES.get(game_name, None)
 
-def set_session(key, game):
-    SESSIONS[key] = game
